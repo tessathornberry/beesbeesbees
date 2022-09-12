46bc4984e798da27a546e0d6dc381321c5c227ba
Tessa Thornberry <tthornberryclass@gmail.com>
2022-09-12 12:00:46 -0700

    pass BeesBeesBees.js

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,31 @@
-var Bee = function() {
+/*Bee
+ Create a Bee class, in pseudoclassical style, with:
+ call the Grub superclass
+ set the prototype
+ set the constructor
+ an age property that is set to 5
+ a color property that is set to yellow
+ a food property that is inherited from grub
+ an eat method that is inherited from grub
+ a job property that is set to keep on growing
+
+ var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
+we have an object grub.prototype
+Grub.prototype.eat = function() {
+  return this.food;
+};
+*/
+var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "keep on growing";
+};
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
 
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,33 @@
-var RetiredForagerBee = function() {
+/* Create a RetiredRetiredForagerBee class, in pseudoclassical style, with:
+ call the RetiredForagerBee superclass
+ set the prototype
+ set the constructor
+ an age property that is set to 40
+ a job property that is set to gamble
+ a canFly property that is set to false
+ a color property that is set to grey
+ a forage method that returns "I am too old, let me play cards instead"
+ a food property that is inherited from grub
+ an eat method that is inherited from grub
+ a treasureChest property inherited from RetiredForagerBee that is set to an empty array []
+ an always winning gamble method that allows the bee to add a treasure to the treasureChest*/
+var RetiredRetiredForagerBee = function() {
 };
 
+var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+};
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+};
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+};
